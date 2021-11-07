80ad8cab21c1b0ac586e77abbfaf88759665a2b9
eligraciel <eliana.bravo@gmail.com>
2021-11-07 23:44:45 +0000

    envio

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
 
-import '@popperjs/core';
-import 'bootstrap/dist/js/bootstrap.bundle';
+//include bootstrap npm library into the bundle
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@popperjs/core";
+import "bootstrap/dist/js/bootstrap.bundle";
 
-import Main from './Main';
+//include your index.scss file into the bundle
+import "./index.css";
 
-ReactDOM.render(<Main />, document.querySelector('#root'));
+//import your own components
+import Layout from "./layout";
+
+//render your react application
+ReactDOM.render(<Layout />, document.querySelector("#app"));
